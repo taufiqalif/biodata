92144bdea96ec145b5ec0ba96aa0997573128b80
taufiqalif <aliftaufiq4@gmail.com>
2020-10-19 19:32:43 +0700

    biodata diri dengan python

---
+++ biodata.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 12:04:23 2020
+
+@author: taufiq
+"""
+
+# Mengambil input
+print "============"
+print "BIODATA"
+print "============"
+
+
+nama = raw_input("Siapa nama kamu: ")
+nama2 = raw_input("nama panggilan:")
+umur = input("Berapa umur kamu: ")
+alamat =raw_input("masukan alamat: ")
+npm = input("masukan no NPM:")
+lahir = raw_input("tempat lahir:")
+tanggl =raw_input("masukan tanggal lahir:")
+no =raw_input("masukan no hp anda:")
+
+
+# Menampilkan output
+
+print "============"
+print "BIODATA SAYA"
+print "============"
+
+
+print "ini adalah biodata diri saya, Nama lengkap saya",nama,",nama panggilan saya",nama2,",NPM saya adalah",npm, ",Saya lahir di",lahir, "tanggal lahir saya",tanggl, "dan umur saya",umur, "tahun Alamat rumah saya di",alamat, "kalian bisa menghubungi saya melalui no ini",no,
